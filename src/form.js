8df5f300197e29e109979a754a23e661a1c361c8
Jiseon Kim <jsid1561@gmail.com>
2021-04-07 17:56:37 +0900

    feature: 다음 버튼  기능 구현

--- src/form.js
+++ src/form.js
@@ -1,9 +1,18 @@
 function scrollDown() {
   const vheight = $(".test").height();
-
   $("html, body").animate(
     {
       scrollTop: (Math.floor($(window).scrollTop() / vheight) + 1) * vheight,
+    },
+    500
+  );
+}
+
+function scrollUp() {
+  const vheight = $(".test").height();
+  $("html, body").animate(
+    {
+      scrollTop: (Math.ceil($(window).scrollTop() / vheight) - 1) * vheight,
     },
     500
   );
@@ -13,5 +22,10 @@ $(function () {
   $(".next_btn").click(function (e) {
     e.preventDefault();
     scrollDown();
+  });
+
+  $(".prev_btn").click(function (e) {
+    e.preventDefault();
+    scrollUp();
   });
 });
